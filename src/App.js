7220a0d290af18d142f4305f8deb6e35170dc200
aditya mishra <adityamishra1304@gmail.com>
2026-09-04 16:54:10 +0530

    Add website header and button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { Fragment } from "react/jsx-runtime";
+import Header from "./Components/Layout/Header";
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment>
+      <Header />
+    </Fragment>
   );
 }
 

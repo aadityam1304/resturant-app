39ebfdaff4f4502cfb7f92aabedb81dc7704c299
aditya mishra <adityamishra1304@gmail.com>
2026-09-04 17:53:11 +0530

    Add website Description and render the menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Fragment } from "react/jsx-runtime";
 import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
